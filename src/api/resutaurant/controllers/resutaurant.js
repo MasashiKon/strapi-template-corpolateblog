e1cd2704c5a219b290e145d9907f5ddaf799fa54
MasahiKon <konnonorth@gmail.com>
2023-06-26 23:02:10 -0700

    first commit

---
+++ src/api/resutaurant/controllers/resutaurant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resutaurant controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::resutaurant.resutaurant');
---
+++ src/api/resutaurant/routes/resutaurant.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * resutaurant router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::resutaurant.resutaurant');
